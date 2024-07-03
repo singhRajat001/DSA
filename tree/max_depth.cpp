aded36284b9bfe461ffd76b7f8319ee31b93b31c
singhRajat001 <164198043+singhRajat001@users.noreply.github.com>
2024-07-03 15:45:23 +0530

    max_depth

--- tree/max_depth.cpp
+++ tree/max_depth.cpp
@@ -17,3 +17,20 @@ public:
         return solve(root);
     }
 };
+
+// ***************************************************************************
+class Solution {
+public:
+
+    int solve(TreeNode* &root, int &maxi){
+        if(root == NULL) return 0;
+
+        int left = solve(root->left, maxi);
+        int right = solve(root->right, maxi);
+        return max(left, right) + 1;
+    }
+    int maxDepth(TreeNode* root) {
+        int maxi = 0;
+        return solve(root, maxi);
+    }
+};
