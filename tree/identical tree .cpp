426058de4539e755ffde30df983cf636d36399d9
singhRajat001 <164198043+singhRajat001@users.noreply.github.com>
2024-07-04 11:48:44 +0530

    Update identical tree

--- tree/identical tree .cpp
+++ tree/identical tree .cpp
@@ -1,9 +1,12 @@
+
+// Simple DFS TRAVERSAL TC->O(N) SC->O(height of tree) of recursion stack space
 class Solution {
 public:
-    // Simple DFS TRAVERSAL TC->O(N) SC->O(height of tree) of recursion stack space
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-        if(!p || !q) return (p == q);
+        // if(both are null) then 1st condition will be satisfied no need to go 2nd if
+        // if(one of them is null) means they are not same 
+        if(p == NULL && q == NULL) return true;
+        // if(p == NULL || q == NULL) return false; for both true above case will execute
         return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
